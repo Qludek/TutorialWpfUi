aed047705b4bbb6a474654dbe456a9106bb53886
marek.zaremba <marek.zaremba@codefusion.pl>
2019-11-07 15:50:16 +0100

    (feature) started faetto project

---
+++ FasettoWord/FasettoWord/ViewModel/BaseViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel;
+
+namespace FasettoWord
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+    }
+}
