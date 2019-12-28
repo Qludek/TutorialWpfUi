444440653ba07c6c79ad2bfdcfb3118fa36f02e5
Marek Zaremba <marek.k.zaremba@protonmail.com>
2019-12-28 16:26:17 +0100

    (feature) added windowChrome, ability to resize and outer margin

--- FasettoWord/FasettoWord/ViewModel/WindowViewModel.cs
+++ FasettoWord/FasettoWord/ViewModel/WindowViewModel.cs
@@ -9,18 +9,59 @@ namespace FasettoWord
     {
         #region PrivateMember
         private Window mWindow;
+
+        private int mOuterMarginSize = 10;
+
+        private int mWindowRadius = 10;
         #endregion
 
         #region Public Properties
         public int ResizeBorder { get; set; } = 6;
 
-        public Thickness ResizeBorderThickness { get { return new Thickness { ResizeBorder }; } }
+        public Thickness ResizeBorderThickness { get { return new Thickness (ResizeBorder + OuterMarginSize); } }
+
+        public int OuterMarginSize
+        {
+            get
+            {
+                return mWindow.WindowState == WindowState.Maximized ? 0 : mOuterMarginSize;
+            }
+            set
+            {
+                mOuterMarginSize = value;
+            }
+        }
+
+        public Thickness OuterMarginSizeThickness { get { return new Thickness(OuterMarginSize); } }
+
+        public int WindowRadius
+        {
+            get
+            {
+                return mWindow.WindowState == WindowState.Maximized ? 0 : mWindowRadius;
+            }
+            set
+            {
+                mWindowRadius = value;
+            }
+        }
         #endregion
+
+        public CornerRadius WindowCornerRadius { get { return new CornerRadius(WindowRadius); } }
 
         #region Constructor
         public WindowViewModel(Window window)
         {
             mWindow = window;
+
+            mWindow.StateChanged += (sender, e) =>
+            {
+                OnPropertyChanged(nameof(ResizeBorderThickness));
+                OnPropertyChanged(nameof(OuterMarginSize));
+                OnPropertyChanged(nameof(OuterMarginSizeThickness));
+                OnPropertyChanged(nameof(WindowRadius));
+                OnPropertyChanged(nameof(WindowCornerRadius));
+            };
         }
 
         #endregion
